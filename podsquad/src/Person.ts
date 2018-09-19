2f9dfb5399cbc2feaf8993474a2e35dc9e9262da
Marc Oelkers <mkoelkers@gmail.com>
2018-09-19 16:58:24 -0500

    Added Favorite to Person Class

--- podsquad/src/Person.ts
+++ podsquad/src/Person.ts
@@ -2,11 +2,13 @@ class Person {
     Name: string;
     Location: string;
     Image: string;
+    IsFavorite: boolean;
 
-    constructor(Name: string, Location: string, Image:string) {
+    constructor(Name: string, Location: string, Image: string, IsFavorite: boolean) {
         this.Name = Name;
         this.Location = Location;
         this.Image = Image;
+        this.IsFavorite = IsFavorite;
     }
     getName() {
         return this.Name;
@@ -16,6 +18,9 @@ class Person {
     }
     getImage() {
         return this.Image;
+    }
+    getIsFavorite(){
+        return this.IsFavorite;
     }
     
 }
