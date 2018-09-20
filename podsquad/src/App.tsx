3c02b433e0f58eb473443bd08d920c0d43802a69
Marc Oelkers <mkoelkers@gmail.com>
2018-09-20 11:10:36 -0500

    Added Note Object

--- podsquad/src/App.tsx
+++ podsquad/src/App.tsx
@@ -8,7 +8,7 @@ import Profile from './Profile';
 
 class App extends React.Component {
   public render() {
-var myPerson : Person= {Name: "Mitch Hussey", Location: "New York", Image: "https://scontent-dfw5-1.xx.fbcdn.net/v/t31.0-8/23275741_10155914503929777_7875720261949730197_o.jpg?_nc_cat=0&oh=44aca1ca4085351b3b0d5580697b8330&oe=5C1D1637", IsFavorite: true} 
+var myPerson : Person= {Id:1 , Name: "Mitch Hussey", Location: "New York", Image: "https://scontent-dfw5-1.xx.fbcdn.net/v/t31.0-8/23275741_10155914503929777_7875720261949730197_o.jpg?_nc_cat=0&oh=44aca1ca4085351b3b0d5580697b8330&oe=5C1D1637", IsFavorite: true} 
 
     return (
       <div className="App">
@@ -21,15 +21,15 @@ var myPerson : Person= {Name: "Mitch Hussey", Location: "New York", Image: "http
         </p>
         <div className="row left contactCard">
         <div className="col l4 m6 hide-on-small-and-down">
-        <Card Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
+        <Card Id={myPerson.Id} Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
         </div>
         <div className="col l4 m6 hide-on-small-and-down">
-        <Card Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
+        <Card Id={myPerson.Id} Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
         </div>
-        <Card Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
+        <Card Id={myPerson.Id} Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
         <Modal/>
         </div>
-        <Profile Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
+        <Profile Id={myPerson.Id} Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite = {myPerson.IsFavorite}/>
       </div>
     );
   }
--- podsquad/src/Person.ts
+++ podsquad/src/Person.ts
@@ -1,11 +1,9 @@
 class Person {
+    Id: number;
     Name: string;
     Location: string;
     Image: string;
     IsFavorite: boolean;
-
-   
-    
 }
 
 export default Person;
