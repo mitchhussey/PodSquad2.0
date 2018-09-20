1016d59f86159dbef1fb29f41589a80f52ec4be7
Marc Oelkers <mkoelkers@gmail.com>
2018-09-20 15:56:49 -0500

    Profile

--- podsquad/src/Profile.tsx
+++ podsquad/src/Profile.tsx
@@ -2,22 +2,38 @@ import * as React from 'react';
 import './materialize.css';
 import './Profile.css';
 import './materialize.js';
+import Note from './Note'
 import Person from './Person';
 
 
-class Modal extends React.Component<Person> {
-  
+interface IProfileProps extends Person{
+    newNote(noteId: number, personId: number, text: string):void
+}
+class Profile extends React.Component<IProfileProps> {
+    text = "";
+    onchange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>{
+        this.text = e.target.value;
+    }
+     addNewNote = (e: React.MouseEvent<HTMLButtonElement>) =>{
+        var myNote: Note = { Id: this.props.Notes.length, PersonId: this.props.Id, Text: this.text };
+        this.props.newNote(this.props.Notes.length, this.props.Id, this.text );
+
+    }
     public render() {
+        var NewNoteText = <textarea id="newNoteText" placeholder="Notes" onChange={this.onchange} className="materialize-textarea"></textarea>
+        var newNoteButton = <button className="btn waves-effect waves-light right blue" type="submit" name="action" onClick={this.addNewNote}><i className="material-icons">note_add</i></button>
 
-        var htmlNotes = this.props.Notes.map(n => 
+
+
+        var htmlNotes = this.props.Notes.map(n =>
             <div id="note" className="row card yellow lighten-4 input-field">
-            {/* <div className="col s8  m9 xl10 input-field"> */}
-            <div className="col s11 input-field">
-                <textarea disabled id="disabledInput" className="validate materialize-textarea">{n.Text}</textarea>
-            </div>
+                {/* <div className="col s8  m9 xl10 input-field"> */}
+                <div className="col s11 input-field">
+                    <textarea disabled id="disabledInput" className="validate materialize-textarea">{n.Text}</textarea>
+                </div>
 
 
-            {/* <div className="col s4 m3  xl2">
+                {/* <div className="col s4 m3  xl2">
               <button className="left btn-floating btn-small waves-effect waves-light blue" type="submit" id="update"
                 name="action">
                 <i className="material-icons right">save</i>
@@ -39,52 +55,49 @@ class Modal extends React.Component<Person> {
                 </li>
               </ul>
             </div> */}
-          </div>
-        
+            </div>
+
         )
 
 
-    return (
-      
-        <div className="container">
-        <div className="row">
-          <div className="col s12 m6">
-            <img className="responsive-img z-depth-4 materialboxed" src={this.props.Image}></img>
-          </div>
-          <div className="col s12 m6">
-            <h3>
-            {this.props.Name}
-            </h3>
-            <blockquote>
-              {this.props.Location}
+        return (
+
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 m6">
+                        <img className="responsive-img z-depth-4 materialboxed" src={this.props.Image}></img>
+                    </div>
+                    <div className="col s12 m6">
+                        <h3>
+                            {this.props.Name}
+                        </h3>
+                        <blockquote>
+                            {this.props.Location}
+                            <br />
+                            C1
               <br />
-              C2
-              <br />
-              University of Texas
+                            BYU
             </blockquote>
-          </div>
-        </div>
-      
-        <div className="row">
-      
-          <div className="input-field col s9 m10 l11">
-            <textarea id="textarea1" placeholder="Notes" className="materialize-textarea"></textarea>
-          </div>
-          <div className="input-field col s3 m2 l1 right">
-            <button className="btn waves-effect waves-light right blue" type="submit" name="action">
-              <i className="material-icons">note_add</i>
-            </button>
-          </div>
-        </div>
-        <div className="notes">
-        {htmlNotes}
-        </div>
-        </div>
+                    </div>
+                </div>
+                <div className="row">
+
+                    <div className="input-field col s9 m10 l11">
+                        {NewNoteText}
+                    </div>
+                    <div className="input-field col s3 m2 l1 right">
+                        {newNoteButton}
+                    </div>
+                </div>
+                <div className="notes">
+                    {htmlNotes}
+                </div>
+            </div>
 
 
 
-    );
-  }
+        );
+    }
 }
 
-export default Modal;
+export default Profile;
