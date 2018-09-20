b1ecd5ac613f6973cb9c2d96bb4682bbed9a715f
Mitch Hussey <mitchhussey@gmail.com>
2018-09-20 14:05:07 -0500

    Made profile dynamic generated

--- podsquad/src/App.tsx
+++ podsquad/src/App.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import './App.css';
 import Card from './Card'
-import logo from './logo.svg';
+
 import Modal from './Modal';
-import Person from './Person';
+// import Person from './Person';
 import Profile from './Profile';
 
 class App extends React.Component {
@@ -13,32 +13,31 @@ class App extends React.Component {
       { Id: 2, Name: "Mark Oelkers", Location: "Dallas", Image: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/13901400_10153363537012255_1903444876443362835_n.jpg?_nc_cat=111&oh=4613ae458dba6efc83040e4fdeb8b66f&oe=5C29A2F9", IsFavorite: true, Notes: [] },
     ];
 
-    let people2 = [];
-    let myPerson: Person = { Id: 2, Name: "Mark Oelkers", Location: "Dallas", Image: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/13901400_10153363537012255_1903444876443362835_n.jpg?_nc_cat=111&oh=4613ae458dba6efc83040e4fdeb8b66f&oe=5C29A2F9", IsFavorite: true, Notes: [] }
-    people2.push(myPerson);
-
-    var htmlCards = people.map(p => <div className="col l4 m6 hide-on-small-and-down">
+    var htmlCards = people.map(p => 
+    <div className="col l4 m6 hide-on-small-and-down">
       <Card Id={p.Id} Name={p.Name} Location={p.Location} Image={p.Image} IsFavorite={p.IsFavorite} Notes={p.Notes} />
-    </div>)
+    </div>);
+
+    var htmlProfile = people.map(p => 
+      <div className="col s12">
+      <Profile Id={p.Id} Name={p.Name} Location={p.Location} Image={p.Image} IsFavorite={p.IsFavorite} Notes={p.Notes} />
+      </div>
+      )
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to PodSqad</h1>
+          
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
         <div className="row left contactCard">
 
           {htmlCards}
-
-          {/* <Card Id={myPerson.Id} Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite={myPerson.IsFavorite} Notes={myPerson.Notes} /> */}
-          <Modal />
+          <hr></hr>
+          {htmlProfile}
+            <Modal />
         </div>
-        <Profile Id={myPerson.Id} Name={myPerson.Name} Location={myPerson.Location} Image={myPerson.Image} IsFavorite={myPerson.IsFavorite} Notes={myPerson.Notes} />
-      </div>
+         </div>
     );
   }
 }
